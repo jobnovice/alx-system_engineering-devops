a7b05dad99b505576f6c3904b51606212d6a2d1b
Eyob <jobkebede47@gmail.com>
2023-11-27 22:33:21 +0300

    let's try this

--- 0x0B-ssh/1-create_ssh_key_pair.sh
+++ 0x0B-ssh/1-create_ssh_key_pair.sh
@@ -1,19 +1,3 @@
-b
-B
-B
-B
-B
-
-A
-A
-A
-A
-A
-B
-B
-B
-B
-B
-A
-C
-#
+#!/usr/bin/env bash
+#create another rsa key pair by specfying the number of bits and the file name
+ssh-keygen -f school -b 4096 -N betty
