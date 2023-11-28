650d84c3d9696dcb5af3bef240336a207d27e105
jobnovice <jobkebede47@gmail.com>
2023-11-28 17:29:44 +0300

    new one

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # my first configuration that defines a new resource
 file { '/tmp/school':
-  ensure     => 'file',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+      ensure     => 'file',
+      mode       => '0744',
+      owner      => 'www-data',
+      group      => 'www-data',
+      content    => 'I love Puppet',
 }
