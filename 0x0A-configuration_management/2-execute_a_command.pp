899fd50cc47e3df3568cdc7c82e9bd4c9fb44224
jobnovice <jobkebede47@gmail.com>
2023-11-28 18:50:07 +0300

    should be exactly one space after it

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -3,6 +3,6 @@
 
 #a puppet manifest that kills process using the exec resource along with the command to pkill
 exec {'killingAprocess':
-    command  => '/usr/bin/pkill -f example_process_name',
-    path     => '/usr/bin:/bin'
+    command => '/usr/bin/pkill -f example_process_name',
+    path    => '/usr/bin:/bin'
 }
