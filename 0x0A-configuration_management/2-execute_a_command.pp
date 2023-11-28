f2454c6e51f8309407f99775fb5adde54db63469
jobnovice <jobkebede47@gmail.com>
2023-11-28 18:40:30 +0300

    now this

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+#a puppet manifest that kills a process using the exec resource with the command attribute
+
+
+#a puppet manifest that kills process using the exec resource along with the command to pkill
+exec {"killingAprocess":
+    command => "pkill -f killmenow"
+}
