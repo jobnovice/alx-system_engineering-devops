2179a7dc5103038dd7004577182aff1df3f14780
jobnovice <jobkebede47@gmail.com>
2023-12-11 23:25:35 +0300

    let's check this

--- 0x0C-web_server/0-transfer_file.sh
+++ 0x0C-web_server/0-transfer_file.sh
@@ -2,8 +2,14 @@
 # a script that copies a file to a remote server
 # accepts four arguments
 
+file_path = $1
+IP = $2
+user_name = $3
+path_to_ssh_key = $4
+detination = ~/
+
 if [$# -lt 4]; then
     echo "Usage: 0-transfer_file PATH_TO_FILE IP USERNAME PATH_TO_SSH_KEY"
 else
-    scp -o StrictHostKeyChecking=no -i $4 $1 $2 $3
+    scp -o StrictHostKeyChecking=no -i "$path_to_ssh_key" "$file_path" "$user_name@$IP:$destination"
 fi
