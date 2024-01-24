925359b1f0f083ce17d8216f0748689dd3bd401d
jobnovice <jobkebede47@gmail.com>
2024-01-24 04:56:44 +0300

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+file { '/home/job/.ssh':
+  ensure => directory,
+  mode   => '0700',
+}
+
+file { '/home/job/.ssh/config':
+  ensure  => present,
+  content => "# Puppet-managed SSH client configuration\n\nHost school-server\n  HostName 100.26.217.2\n  User job\n  IdentityFile /home/job/.ssh/school\n  PasswordAuthentication no\n",
+  mode    => '0600',
+}
