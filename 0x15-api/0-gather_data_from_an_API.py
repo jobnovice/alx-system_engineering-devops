297bba54de2eacc5281ddc7ac5f7738ab1e632f5
jobnovice <jobkebede47@gmail.com>
2024-03-30 19:56:13 +0300

    aight let's check it out

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,19 +4,30 @@
 import requests
 import sys
 
-if (len(sys.argv) == 2):
-	if type(sys.argv[1]) != int and sys.argv[1] > 11 and sys.argv[1] < 0:
-		print("Not a valid number")
-		exit()
-else:
-	print("invalid input need to give us the employee ID")
-	exit()	
+if len(sys.argv) != 2:
+    print("Invalid input. Please provide the employee ID.")
+    sys.exit(1)
 
-response = requests.get("https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos")
-r2 = requests.get("https://jsonplaceholder.typicode.com/users/{sys.argv[1]}")
+try:
+    employee_id = int(sys.argv[1])
+except ValueError:
+    print("Employee ID must be an integer.")
+    sys.exit(1)
+
+response = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+                        {employee_id}/todos")
+r2 = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+                  {employee_id}")
 data = response.json()
 d2 = r2.json()
 
-print("Employee {d2.get('name')} is done with tasks({len(data.get('completed'))}/{len(data)})")
+completed_task = 0
+for i in data:
+	if i["completed"]:
+		completed_task += 1
 
+print(f"Employee {d2['name']} is done with tasks({completed_task}/{len(data)}):")
 
+for i in data:
+     if i["completed"]:
+           print("\t ", i["title"])
