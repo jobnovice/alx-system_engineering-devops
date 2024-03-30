720c1f28ae5e3009fa57e16f0b7fdd05fe07c2e6
jobnovice <jobkebede47@gmail.com>
2024-03-30 19:58:31 +0300

    eshi

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """using API to get data and display it in a specific format"""
 
-import requests
 import sys
+import requests
 
 if len(sys.argv) != 2:
     print("Invalid input. Please provide the employee ID.")
