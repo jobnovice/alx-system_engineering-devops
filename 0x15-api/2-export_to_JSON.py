e032dd7892e1fe5aaca197f114e1eb0eb447adfb
jobnovice <jobkebede47@gmail.com>
2024-04-13 00:59:27 +0300

    checked too

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,47 @@
 #!/usr/bin/python3
-"""using what we did earlier we now have to export the data to json format"""
+"""Export data to JSON format"""
 
 import sys
 import requests
 import json
 
-if (len(sys.argv) < 2):
-    print("gotta provide more arguments")
+if len(sys.argv) < 2:
+    print("Usage: python script.py <user_id>")
     exit(1)
+
 user_id = int(sys.argv[1])
 
+# Fetch user data
+user_response = requests.get(f'https://jsonplaceholder' +
+                             f'.typicode.com/users/{user_id}')
+if user_response.status_code != 200:
+    print("Failed to fetch user data.")
+    exit(1)
+user = user_response.json()
 
+# Fetch todos data
+todos_response = requests.get(f'https://jsonplaceholder' +
+                              f'.typicode.com/todos?userId={user_id}')
+if todos_response.status_code != 200:
+    print("Failed to fetch todos data.")
+    exit(1)
+todos = todos_response.json()
 
-user =  requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
-todos = requests.get(f'https://jsonplaceholder.typicode.com/todos?{user_id}').json()
+# Construct data dictionary
+data = {
+    str(user_id): []
+}
 
-# data = {
-#     for todo in todos:
-#         "tasks":   
-# }
+# Add todos to the tasks list
+for todo in todos:
+    task = {
+        "task": todo["title"],
+        "completed": todo["completed"],
+        "username": user["username"]
+    }
+    data[str(user_id)].append(task)
 
+# Export data to JSON file
 filename = f'{user_id}.json'
-
 with open(filename, 'w') as file:
-    json.dump(f'{user_id}:' + 
-                [{"task": todos["title"],
-                  "completed": todos["completed"],
-                  "username": user["username"]}], filename, indent=2)
+    json.dump(data, file)
