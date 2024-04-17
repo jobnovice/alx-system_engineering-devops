c4e1f1029cd84e98321e0c82779c90fd255b1725
jobnovice <jobkebede47@gmail.com>
2024-04-17 15:02:33 +0300

    did it change

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,6 @@
-# Puppet manifest to fix Apache 500 error
+# Fixes bad phpp extensions to php in the WordPress file wp-settings.php.
 
-# Define variables
-$apache_conf_dir = '/etc/apache2/sites-available'
-$virtual_host_file = 'example.com.conf' # Replace with the actual name of your virtual host file
-
-# Ensure the virtual host file is present and contains correct configuration
-file { "${apache_conf_dir}/${virtual_host_file}":
-  ensure  => file,
-  content => template('apache/virtual_host.conf.erb'), # You'll need to create this template with the correct configuration
-  notify  => Service['apache2'], # Restart Apache service when the configuration file changes
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Ensure Apache service is running and enabled
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
-
