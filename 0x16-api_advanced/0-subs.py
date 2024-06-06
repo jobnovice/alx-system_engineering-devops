5b3229e7fbeaa914a6e32c8e58a67d02d957e6b5
jobnovice <jobkebede47@gmail.com>
2024-06-07 01:30:24 +0300

    eshi

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""module deined for fetching the number of subscrbribers for a subreddit"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/"
-
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
     try:
         response = requests.get(url, allow_redirects=False)
+        if response.status_code == 200:
+            res = response.json()
+            return res['subscribers']
+        else:
+            return 0
 
-        res = response.json()
-
-        return res['data']['subscribers']
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException:
         return 0
