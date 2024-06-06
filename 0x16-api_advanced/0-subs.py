eadb23333dd3e5d6e2bcd6c0767de39ce4bf8e1d
jobnovice <jobkebede47@gmail.com>
2024-06-07 01:54:00 +0300

    headers??

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,7 +17,10 @@ def number_of_subscribers(subreddit):
 
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, allow_redirects=False)
+    headers = {'User-Agent': 'YOUR_USER_AGENT'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
