be39aef150b8cf04e65cd49a9823f0e0479b1539
jobnovice <jobkebede47@gmail.com>
2024-06-07 01:43:06 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
+"""created a module that accpets a subreddit as a parameter returns the number
+of subscriber"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+             Returns 0 if the subreddit does not exist or if there is an error.
+
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     try:
