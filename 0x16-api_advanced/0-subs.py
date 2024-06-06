66d2976f1220f63c68861f3eeb827b4f30ae910e
jobnovice <jobkebede47@gmail.com>
2024-06-07 01:48:25 +0300

    noice

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,9 +17,9 @@ def number_of_subscribers(subreddit):
 
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MyRedditClient/1.0)'}
     try:
-        response = requests.get(url, allow_redirects=False)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             res = response.json()
             return res['data']['subscribers']
