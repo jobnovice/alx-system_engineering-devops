a6701005573aef405095bc50c2fe31ae01162e1d
jobnovice <jobkebede47@gmail.com>
2024-06-07 01:00:37 +0300

    what

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-"""using the api module to fetch information about"""
+"""module deined for fetching the number of subscrbribers for a subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/"
+
+    try:
+        response = requests.get(url, allow_redirects=False)
+
+        res = response.json()
+
+        return res['data']['subscribers']
+    except requests.exceptions.RequestException as e:
+        return 0
