1af3ce63a37cf1d2b5e3a7058721827f406010b9
jobnovice <jobkebede47@gmail.com>
2024-06-07 01:52:34 +0300

    simplified

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,14 +17,9 @@ def number_of_subscribers(subreddit):
 
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MyRedditClient/1.0)'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            res = response.json()
-            return res['data']['subscribers']
-        else:
-            return 0
-
-    except requests.exceptions.RequestException:
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
